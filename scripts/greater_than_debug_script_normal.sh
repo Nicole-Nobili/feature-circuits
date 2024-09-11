550a2eb62acd965a750c462da34c0c48ffa3d122
nobinic <nicolenobili17@gmail.com>
2024-09-11 22:56:57 +0000

    implementing control task for fake

--- scripts/greater_than_debug_script_normal.sh
+++ scripts/greater_than_debug_script_normal.sh
@@ -1,1 +1,1 @@
-python circuit.py --model EleutherAI/pythia-70m-deduped --num_examples 100 --batch_size 10 --dataset greater_than_examples --node_threshold 0.1 --aggregation none --example_length 12
+python circuit.py --model EleutherAI/pythia-70m-deduped --num_examples 100 --batch_size 10 --dataset greater_than_examples_fake --node_threshold 0.1 --aggregation none --example_length 12
